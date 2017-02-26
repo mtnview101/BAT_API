78acb20d1165bd1b4ceba9148ba102c35e4a1966
mtnview101 <mtnview101@gmail.com>
2017-02-25 18:57:11 -0800

    commit 3

---
+++ src/test/java/core/XSDValidationTest_0.java
@@ -0,0 +1,14 @@
+package core;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;	
+import org.testng.*;
+import org.testng.annotations.*;
+
+public class XSDValidationTest_0 {
+       @BeforeMethod(alwaysRun = true)
+       public void bm()    {}
+       @Test
+       public void test() {
+              assertThat(1, equalTo(1));}}
+
